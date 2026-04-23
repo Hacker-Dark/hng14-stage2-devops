23ca5a23ec1b7233f238c818db2efaafdd878e35
Mordecai Ojochenemi Amehson <87395152+Hacker-Dark@users.noreply.github.com>
2026-04-23 00:26:36 +0000

    style(api): fix flake8 formatting issues

--- api/main.py
+++ api/main.py
@@ -30,4 +30,4 @@ def get_job(job_id: str):
     status = r.hget(f"job:{job_id}", "status")
     if not status:
         raise HTTPException(status_code=404, detail="Job not found")
-    return {"job_id": job_id, "status": status.decode()}
+    return {"job_id": job_id, "status": status.decode()} 
